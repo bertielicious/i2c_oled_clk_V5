6df9f646ef3d0403099ecca5f14d4f20f88130a1
Phil Glazzard <phil@hifigear.co.uk>
2019-03-04 19:11:11 +0000

    Add files via upload

---
+++ read_temperature.c
@@ -0,0 +1,33 @@
+#include "config.h"
+#include "ow_reset.h"
+#include "write_byte.h"
+#include "read_byte.h"
+#include <stdio.h>
+void read_temperature (void)
+{
+char get[10];
+char temp_lsb,temp_msb;
+int k;
+char temp_f,temp_c;
+ow_reset();
+write_byte(0xCC); //Skip ROM
+write_byte(0x44); // Start Conversion
+__delay_us(104);
+ow_reset();
+write_byte(0xCC); // Skip ROM
+write_byte(0xBE); // Read Scratch Pad
+for (k=0;k<9;k++){get[k]=read_byte();}
+//printf("\n ScratchPAD DATA = %X%X%X%X%X\n",get[8],get[7],get[6],get[5],get[4],get[3],get[2],get[1],get[0]);
+temp_msb = get[1]; // Sign byte + lsbit
+temp_lsb = get[0]; // Temp data plus lsb
+if (temp_msb <= 0x80){temp_lsb = (temp_lsb/2);} // shift to get whole degree
+temp_msb = temp_msb & 0x80; // mask all but the sign bit
+if (temp_msb >= 0x80) {temp_lsb = (~temp_lsb)+1;} // twos complement
+if (temp_msb >= 0x80) {temp_lsb = (temp_lsb/2);}// shift to get whole degree
+if (temp_msb >= 0x80) {temp_lsb = ((-1)*temp_lsb);} // add sign bit
+//printf( "\nTempC= %d degrees C\n", (int)temp_lsb ); // print temp. C
+temp_c = temp_lsb; // ready for conversion to Fahrenheit
+temp_f = (((int)temp_c)* 9)/5 + 32;
+//printf( "\nTempF= %d degrees F\n", (int)temp_f ); // print temp. F
+celcius = temp_f/4;//(temp_msb|temp_lsb)/2;
+}
